bb1bdefb4ecf2ea7da2eb652158a4564f6449b35
Daniel Welsh <d.welsh@newcastle.ac.uk>
2018-08-20 15:37:40 +0100

    :sparkles: Adding form for submitting story

---
+++ src/components/story/NewStory.vue
@@ -0,0 +1,165 @@
+<template lang="pug">
+  div#new-story
+    div.container
+      h1.title.has-text-right Your Story
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Story
+        div.field-body
+          div.field
+            div.control
+              textarea.textarea(
+                placeholder="Textarea"
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Start
+        div.field-body
+          div.field
+            div.control
+              input.input(
+                type="text",
+                placeholder="Start"
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label End
+        div.field-body
+          div.field
+            div.control
+              input.input(
+                type="text",
+                placeholder="End"
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Message to a Stranger
+        div.field-body
+          div.field
+            div.control
+              textarea.textarea(
+                placeholder="Textarea"
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Thank You Note
+        div.field-body
+          div.field
+            div.control
+              textarea.textarea(
+                placeholder="Textarea"
+                )
+
+    div#expenses.container
+      h1.title.has-text-right Expenses
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Procedure
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Travel
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Food
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Childcare
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Accommodation
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Other Expenses
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Paid Days Missed
+        div.field-body
+          div.field
+            div.control
+              input.input.has-text-right(
+                type="number"
+                placeholder="1000",
+                )
+
+      div.field.is-horizontal
+        div.field-label.is-normal
+          label.label Currency
+        div.field-body
+          div.field.is-narrow
+            div.control
+              div.select.is-fullwidth
+                select
+                  option £
+                  option €
+
+      div.field.is-horizontal
+        div.field-label
+        div.field-body
+          div.field
+            div.control
+              button.button.is-primary Submit Story
+</template>
+
+<script>
+export default {
+  name: "NewStory"
+}
+</script>
+
+<style lang="scss">
+#expenses {
+  margin-top: 5%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
   div.home
     Title
+    NewStory
 </template>
 
 <script>
 // @ is an alias to /src
 import Title from "@/components/Title.vue";
+import NewStory from "@/components/story/NewStory.vue"
 
 export default {
   name: "home",
   components: {
-    Title
+    Title,
+    NewStory
   }
 };
 </script>
