a9494945753c9d48badd1ed325220772334b3de6
Daniel Welsh <d.welsh@newcastle.ac.uk>
2018-09-05 11:25:49 +0100

    :rotating_light: Linting api helpers

--- src/utils/api/stories.js
+++ src/utils/api/stories.js
@@ -7,6 +7,5 @@ import Axios from "axios";
  */
 export async function getPublicStories() {
   const response = await Axios.get(`${URL}/story/public`);
-  const stories = response.data.payload;
-  return stories;
+  return response.data.payload;
 }
